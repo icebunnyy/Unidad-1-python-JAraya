5a6197f43ec735829cadbec8a72628821a110b55
Laboratorio Computación N° 9 <claseslab9@inacap.cl>
2025-08-29 11:30:04 -0400

    mi primer commit

--- monitoreo/dispositivos/admin.py
+++ monitoreo/dispositivos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Categoria, Zona, Dispositivo
+
+admin.site.register([Categoria, zona])
+
+
+admin.site.register(Dispositivo)
